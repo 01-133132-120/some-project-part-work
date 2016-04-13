5d2b74449601dc476312d601c67ff809b00a7420
hassan <hassan26khan@gmail.com>
2016-04-14 00:49:33 +0500

    Some part of project check

---
+++ dynamicTextbox and labels/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace dynamicTextbox_and_labels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 101);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(552, 31);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 138);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(552, 68);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 223);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(552, 100);
+            this.panel3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ascending";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Decending";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(329, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Random";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 354);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ dynamicTextbox and labels/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dynamicTextbox_and_labels
+{
+    public partial class Form1 : Form
+    {
+
+        int x = 0;
+        int y = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        TextBox[] mybox = new TextBox[5];
+        Label [] mylabel = new Label[5];
+        //int value;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+             
+            for (int i = 0; i < 5; i++)
+            {
+                mybox[i] = new TextBox();
+                mybox[i].Location = new Point(x, y);
+                mybox[i].Width = 50;
+                x = x + 50;
+                panel1.Controls.Add(mybox[i]);
+                mybox[i].Visible = true;
+                
+            }
+            
+           
+            //single label created
+            
+                /*mylabel = new Label();
+                mylabel.Width = 80;
+                mylabel.Height = 80;
+                mylabel.BackColor = Color.White;
+                panel2.Controls.Add(mylabel);
+                mylabel.Visible = true;*/
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           for (int i = 0; i < 5; i++)
+            {
+                mylabel[i] = new Label();
+                mylabel[i].Location = new Point(a,b );
+                mylabel[i].Text = mybox[i].Text;
+                a= a + 100;
+                mylabel[i].Width = 80;
+                mylabel[i].Height = 80;
+                mylabel[i].BackColor = Color.White;
+                mylabel[i].TextAlign = ContentAlignment.MiddleCenter;
+                panel2.Controls.Add(mylabel[i]);
+               mylabel[i].Visible = true;
+            }   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int val = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                mybox[i].Text = val.ToString();
+                val++;
+            }
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int val1 = 5;
+            for (int i = 0; i < 5; i++)
+            {
+                mybox[i].Text = val1.ToString();
+                val1--;
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                mybox[i].Text = ran.Next(0, 200).ToString();
+            }
+
+           
+        }
+    }
+}
